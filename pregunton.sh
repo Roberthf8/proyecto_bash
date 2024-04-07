2158210aff1f8a0a5b76f4b010b2c8de5e1bbf3c
Roberthf8 <rob.mv.888@gmail.com>
2024-04-07 11:10:08 -0600

    parametrizar el pais

--- pregunton.sh
+++ pregunton.sh
@@ -1,7 +1,8 @@
 #!/bin/Bash
+elpais="USA"
 echo "Hola, yo pregunto cosas"
-echo "Cual es la poblacion de Mexico?"
+echo "Cual es la poblacion de $elpais?"
 grep "Mexico" paises.txt |  cut -d "," -f2
-echo "Cual es el clima de Mexico?"
-grep "Mexico" paises.txt |  cut -d "," -f3
+echo "Cual es el clima de $elpais?"
+grep "$elpais" paises.txt |  cut -d "," -f3
 
